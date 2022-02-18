113581de447f2e9f90ca110dbd8c7450047f10a7
Dillon Winchell <dwinchel@redhat.com>
2022-02-18 02:33:22 -0500

    Update test script with new name of java-maven pipeline

--- hack/test.sh
+++ hack/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-oc create -f test/pipelineruns/ci-java.yml
+oc create -f test/pipelineruns/java-maven.yml
 sleep 1
 tkn pipelinerun describe --last -o yaml | yq .status.conditions
 
