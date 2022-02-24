a7d9f917a1d523775e7c410bec3a41fd78d7a3f8
Dillon Winchell <dwinchel@redhat.com>
2022-02-24 14:13:11 -0500

    Fix namespace in java-maven trigger template

--- components/extras/example-apps/buildah-and-oc/run/run-pipeline.sh
+++ components/extras/example-apps/buildah-and-oc/run/run-pipeline.sh
@@ -1,7 +1,16 @@
 tkn pipeline start build-and-deploy \
-    -w name=shared-workspace,volumeClaimTemplateFile=03_persistent_volume_claim.yaml \
-    -p deployment-name=pipelines-vote-api \
-    -p git-url=https://github.com/openshift/pipelines-vote-api.git \
-    -p IMAGE=image-registry.openshift-image-registry.svc:5000/pipelines-tutorial/pipelines-vote-api \
+    -w name=shared-workspace,volumeClaimTemplateFile=pvc.yaml \
+    -p deployment-name=pipelines-vote-ui \
+    -p git-url=https://github.com/ploigos-reference-apps/pipelines-vote-ui.git \
+    -p IMAGE=image-registry.openshift-image-registry.svc:5000/quickstart-app-buildah-and-oc/pipelines-vote-ui \
     -p TLSVERIFY=false \
     --use-param-defaults
+
+#tkn pipeline start build-and-deploy \
+#    -w name=shared-workspace,volumeClaimTemplateFile=pvc.yaml \
+#    -p deployment-name=pipelines-vote-api \
+#    -p git-url=https://github.com/ploigos-reference-apps/pipelines-vote-api.git \
+#    -p IMAGE=image-registry.openshift-image-registry.svc:5000/quickstart-app-buildah-and-oc/pipelines-vote-api \
+#    -p TLSVERIFY=false \
+#    --use-param-defaults
+
