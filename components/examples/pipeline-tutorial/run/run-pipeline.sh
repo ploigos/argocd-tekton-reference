558c5e398a1b22e1216bd4b522289c198369cbdc
Dillon Winchell <dwinchel@redhat.com>
2022-02-23 22:50:48 -0500

    Fix run script syntax

--- components/examples/pipeline-tutorial/run/run-pipeline.sh
+++ components/examples/pipeline-tutorial/run/run-pipeline.sh
@@ -3,5 +3,5 @@ tkn pipeline start build-and-deploy \
     -p deployment-name=pipelines-vote-api \
     -p git-url=https://github.com/openshift/pipelines-vote-api.git \
     -p IMAGE=image-registry.openshift-image-registry.svc:5000/pipelines-tutorial/pipelines-vote-api \
-    -p TLSVERIFY=false
+    -p TLSVERIFY=false \
     --use-param-defaults
