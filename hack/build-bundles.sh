0a6c1d71a6b5dc891e7f427b99003a1d7fc3a79b
Dillon Winchell <dwinchel@redhat.com>
2022-02-24 21:46:04 -0500

    Add java-maven-cd-pipeline bundle

--- hack/build-bundles.sh
+++ hack/build-bundles.sh
@@ -4,15 +4,16 @@ set -ex
 IMAGE_REGISTRY=quay.io
 IMAGE_REGISTRY_USER=dwinchell_redhat
 IMAGE_TAG=latest
+BUNDLES_DIR=bundles
 
 # Build pipeline bundles
-for YAML_FILE in $(ls resources/pipelines/*.yml); do
+for YAML_FILE in $(ls ${BUNDLES_DIR}/*.yml); do
   TASK_NAME=$(basename ${YAML_FILE} | sed 's/\(.*\)\.yml/\1/')
   tkn bundle push ${IMAGE_REGISTRY}/${IMAGE_REGISTRY_USER}/${TASK_NAME}-bundle:${IMAGE_TAG} -f ${YAML_FILE}
 done
 
 # Build task bundles
-for YAML_FILE in $(ls resources/tasks/*.yml); do
+for YAML_FILE in $(ls ${BUNDLES_DIR}/*.yml); do
   TASK_NAME=$(basename ${YAML_FILE} | sed 's/\(.*\)\.yml/\1/')
   tkn bundle push ${IMAGE_REGISTRY}/${IMAGE_REGISTRY_USER}/${TASK_NAME}-task-bundle:${IMAGE_TAG} -f ${YAML_FILE}
 done
