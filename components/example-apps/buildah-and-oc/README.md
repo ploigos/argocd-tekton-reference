91a77033a9651454605b5942ea230640a3f8de6f
Dillon Winchell <dwinchel@redhat.com>
2022-03-03 23:42:15 -0500

    Restore buildah-and-oc example

---
+++ components/example-apps/buildah-and-oc/README.md
@@ -0,0 +1,7 @@
+
+## Related Tutorial
+This example is loosely based on the tutorial at
+https://docs.openshift.com/container-platform/4.9/cicd/pipelines/creating-applications-with-cicd-pipelines.html#assembling-a-pipeline_creating-applications-with-cicd-pipelines
+
+## Running the Pipeline Manually
+Use ./run/run-pipeline.sh to start the pipeline without configuring a webhook in your git repository.
---
+++ components/example-apps/buildah-and-oc/run/run-pipeline.sh
@@ -0,0 +1,7 @@
+tkn pipeline start build-and-deploy \
+    -w name=shared-workspace,volumeClaimTemplateFile=03_persistent_volume_claim.yaml \
+    -p deployment-name=pipelines-vote-api \
+    -p git-url=https://github.com/openshift/pipelines-vote-api.git \
+    -p IMAGE=image-registry.openshift-image-registry.svc:5000/pipelines-tutorial/pipelines-vote-api \
+    -p TLSVERIFY=false \
+    --use-param-defaults
