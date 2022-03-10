02ff7d83f62143974acd5654409d4bfa3cff169e
Dillon Winchell <dwinchel@redhat.com>
2022-03-10 13:13:31 -0500

    Update easymode test script

--- components/example-apps/easymode/run/run-pipeline.sh
+++ components/example-apps/easymode/run/run-pipeline.sh
@@ -1,7 +1,7 @@
-tkn pipeline start build-and-deploy \
-    -w name=shared-workspace,volumeClaimTemplateFile=03_persistent_volume_claim.yaml \
+tkn pipeline start easymode \
+    -w name=shared-workspace,volumeClaimTemplateFile=test-pvc.yml \
     -p deployment-name=pipelines-vote-api \
     -p git-url=https://github.com/openshift/pipelines-vote-api.git \
-    -p IMAGE=image-registry.openshift-image-registry.svc:5000/pipelines-tutorial/pipelines-vote-api \
+    -p IMAGE=image-registry.openshift-image-registry.svc:5000/easymode/pipelines-vote-api \
     -p TLSVERIFY=false \
     --use-param-defaults
