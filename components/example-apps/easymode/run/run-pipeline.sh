641ceaa04967481805784fef56cbb9b878689301
Dillon Winchell <dwinchel@redhat.com>
2022-03-10 21:28:12 -0500

    Change easymode test script output image to something we have permission to write to

--- components/example-apps/easymode/run/run-pipeline.sh
+++ components/example-apps/easymode/run/run-pipeline.sh
@@ -2,6 +2,6 @@ tkn pipeline start easymode \
     -w name=shared-workspace,volumeClaimTemplateFile=test-pvc.yml \
     -p deployment-name=pipelines-vote-api \
     -p git-url=https://github.com/openshift/pipelines-vote-api.git \
-    -p IMAGE=image-registry.openshift-image-registry.svc:5000/easymode/pipelines-vote-api \
+    -p IMAGE=image-registry.openshift-image-registry.svc:5000/quickstart-app-easymode/pipelines-vote-api \
     -p TLSVERIFY=false \
     --use-param-defaults
